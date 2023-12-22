07db9c7bca8f105672ff9a16957908bcfaa9e255
vitmonjo <vitmonjo@gmail.com>
2023-12-22 00:26:49 -0300

    finish starship fetching

---
+++ src/modules/fetchStarships.js
@@ -0,0 +1,34 @@
+import { countInstances, checkExistence, getInstances } from "./localStorage"
+
+export async function fetchStarships() {
+
+    let starshipsArray = [];
+
+    const starshipsImageMap = {
+        'CR90 corvette': 'https://i.imgur.com/JzcsifG.jpg',
+        'Star Destroyer': 'https://i.imgur.com/0kDcjKh.jpg',
+        'Sentinel-class landing craft': 'https://i.imgur.com/n57oAzP.jpg',
+        'Death Star': 'https://i.imgur.com/8Kfoa5Z.jpg',
+        'Millennium Falcon': 'https://i.imgur.com/OtSNxmm.jpg',
+        'Y-wing': 'https://i.imgur.com/Ga8DJ2Y.jpg',
+        'X-wing': 'https://i.imgur.com/Zw4rQNA.jpg',
+        'TIE Advanced x1': 'https://i.imgur.com/eoi4WWw.jpg',
+        'Executor': 'https://i.imgur.com/iWLUQyy.jpg',
+        'Rebel transport': 'https://i.imgur.com/H9mktfh.jpg',
+        'Slave 1': 'https://i.imgur.com/MAt7hmF.jpg',
+        'Imperial shuttle': 'https://i.imgur.com/sHfsxFx.jpg'
+    }
+
+    const starshipsFetching = async () => {
+        for (let i = 1; i < 5; i++) {
+            let response = await fetch(`https://swapi.dev/api/starships/?page=${i}`);
+            let starshipsData = await response.json();
+            starshipsArray = starshipsArray.concat(starshipsData.results);
+        }
+        return starshipsArray;
+    }
+
+    const starships = await starshipsFetching();
+
+    return {starshipsImageMap, starships};
+}
