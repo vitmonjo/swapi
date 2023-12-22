1296cd7c2ddab16f5296e559caafd5162c7c1df3
vitmonjo <vitmonjo@gmail.com>
2023-12-21 21:33:44 -0300

    finish pilots dom manipulation

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -15,5 +15,19 @@ export function setTabs() {
 }
 
 fetchPilots().then(function(pilots) {
-    console.log(pilots);
+    const cardOrganizer = document.querySelector('.card-organizer');
+    const loading = document.querySelector('.loading');
+    for (let i = 0; i <= pilots.pilots.length - 1; i++) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const pilotName = document.createElement('div');
+        const placeHolder = document.createElement('img');
+        placeHolder.classList.add('placeholder');
+        placeHolder.src = (pilots.pilotImageMap[pilots.pilots[i].name] || '../images/placeholder.jpeg');
+        pilotName.classList.add('element-name');
+        pilotName.textContent = pilots.pilots[i].name;
+        card.append(placeHolder, pilotName);
+        cardOrganizer.append(card);
+        loading.textContent = '';
+    }
 });
