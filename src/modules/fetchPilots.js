256ab082833bdcec805f52e61d2ee244fb34880b
vitmonjo <vitmonjo@gmail.com>
2023-12-24 21:19:46 -0300

    change pilot fetching logic so local storage functions work properly

--- src/modules/fetchPilots.js
+++ src/modules/fetchPilots.js
@@ -1,4 +1,4 @@
-import { countInstances, checkExistence, getInstances } from "./localStorage"
+import { checkExistence, getInstances, storeRecords } from "./localStorage"
 
 export async function fetchPilots() {
 
@@ -10,43 +10,45 @@ export async function fetchPilots() {
         'Darth Vader': 'https://i.imgur.com/QdpvFa9.jpeg',
         'Biggs Darklighter': 'https://i.imgur.com/WfpdZOy.jpeg',
         'Obi-Wan Kenobi': 'https://i.imgur.com/u7PcBAj.jpeg',
-        'Anakin Skywalker': 'https://i.imgur.com/hNEaTgk.jpeg',
+        'Anakin Skywalker': 'https://i.imgur.com/BFKB0Rd.png',
         'Chewbacca': 'https://i.imgur.com/Pie2Fhs.jpeg',
         'Han Solo': 'https://i.imgur.com/8XMRnPY.jpeg',
         'Wedge Antilles': 'https://i.imgur.com/WEVwjCw.jpeg',
         'Jek Tono Porkins': 'https://i.imgur.com/A17GELJ.jpeg',
         'Boba Fett': 'https://i.imgur.com/U34JjDA.jpeg',
-        'Lando Calrissian': 'https://i.imgur.com/IMKaLSf.jpeg',
+        'Lando Calrissian': 'https://i.imgur.com/GFoWhJj.png',
         'Arvel Crynyd': 'https://i.imgur.com/bQzRXMM.png',
         'Nien Nunb': 'https://i.imgur.com/8PsLTCQ.jpeg',
         'Padmé Amidala': 'https://i.imgur.com/rkw33wn.jpeg',
-        'Ric Olié': 'https://i.imgur.com/uFLBrfq.jpeg',
-        'Darth Maul': 'https://i.imgur.com/kTfe7uC.jpeg',
-        'Plo Koon': 'https://i.imgur.com/bH6VnZ2.jpeg',
+        'Ric Olié': 'https://i.imgur.com/s0azTB9.png',
+        'Darth Maul': 'https://i.imgur.com/ankt0IR.png',
+        'Plo Koon': 'https://i.imgur.com/wuS0Mb1.png',
         'Gregar Typho': 'https://i.imgur.com/IvB8RQz.jpeg',
         'Grievous': 'https://i.imgur.com/seoI0lj.png',
     }
 
-    const peopleFetching = async () => {
+    if (checkExistence('PILOT')) pilotsArray = getInstances('PILOT');
+
+    if (pilotsArray.length > 0) return {pilotImageMap, pilotsArray}
+
+    const pilotsFetching = async () => {
         for (let i = 1; i < 10; i++) {
             let response = await fetch(`https://swapi.dev/api/people/?page=${i}`);
             let peopleData = await response.json();
-            peopleArray = peopleArray.concat(peopleData.results)
-        }
-        return peopleArray;
-    }
-
-    const addToPilots = async () => {
-        const data = await peopleFetching();
-        pilotsArray = data.filter(function (person) {
-            if (person.starships.length !== 0) {
-                return true;
+            for (let i = 0; i < peopleData.results.length; i++) {
+                if (peopleData.results[i].starships.length !== 0) {
+                    pilotsArray.push(peopleData.results[i].name);
+                }
             }
-        })
+        }
         return pilotsArray;
     }
 
-    const pilots = await addToPilots();
+    let pilots = await pilotsFetching();
 
-    return {pilotImageMap, pilots};
+    storeRecords(pilots, 'PILOT');
+
+    pilotsArray = pilots;
+
+    return {pilotImageMap, pilotsArray};
 }
